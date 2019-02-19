7ec83948fad968a66d60a6434e2a062ec7816522
Will Lu <willorwgl@gmail.com>
2019-02-19 17:57:38 -0500

    added exception handling

--- src/main/java/com/william/news/controller/ControllerHelper.java
+++ src/main/java/com/william/news/controller/ControllerHelper.java
@@ -1,9 +1,11 @@
 package com.william.news.controller;
 
+import com.william.news.restclient.NewsApiException;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 
 @Controller
@@ -14,6 +16,11 @@ public class ControllerHelper {
     public void initBinder(WebDataBinder dataBinder) {
         StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
         dataBinder.registerCustomEditor(String.class , stringTrimmerEditor);
+    }
+
+    @ExceptionHandler(NewsApiException.class)
+    public String handleException() {
+        return "error";
     }
 
 }
